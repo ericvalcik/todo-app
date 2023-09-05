d16e43c9447bcde208a77e713530cd4db5e9356b
eric valcik <eric.valcik@gmail.com>
2023-09-05 21:27:14 +0200

    Add drizzle and local sqlite

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+  id: integer('id').primaryKey(),
+  fullName: text('full_name'),
+  phone: text('phone'),
+})
+
+export type User = typeof users.$inferSelect // return type when queried
+export type InsertUser = typeof users.$inferInsert // insert type
