f59335c642f2ccd1911634a6c035547e9ed45885
eric valcik <eric.valcik@gmail.com>
2023-09-06 15:35:35 +0200

    Added create new user form - looking pretty good

--- src/pages/add-user.ts
+++ src/pages/add-user.ts
@@ -1,13 +1,32 @@
 import type { APIRoute } from "astro";
+import { type InsertUser, users } from "../db/schema";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: import.meta.env.DATABASE_URL,
+  authToken: import.meta.env.DATABASE_AUTH_TOKEN,
+});
+const db = drizzle(client);
+const insertUser = async (user: InsertUser) => {
+  return await db.insert(users).values(user).execute();
+};
 
 export const POST: APIRoute = async ({ params, request }) => {
-  const body = await request.formData();
-  console.log(body);
+  const data = await request.formData();
+  console.log(data);
 
-  return new Response(
-    JSON.stringify({
-      name: "Astro",
-      url: "https://astro.build/",
-    }),
-  );
+  if (
+    !data.has("fullName") ||
+    !data.get("fullName") ||
+    !data.has("phone") ||
+    !data.get("phone")
+  ) {
+    return new Response("Missing fullName or phone", { status: 400 });
+  }
+  await insertUser({
+    fullName: (data.get("fullName") ?? "").toString(),
+    phone: (data.get("phone") ?? "").toString(),
+  });
+  return new Response("User inserted :)");
 };
