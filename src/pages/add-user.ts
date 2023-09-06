02d29ec6043898609c52928c1d96d5ee896cc730
eric valcik <eric.valcik@gmail.com>
2023-09-06 15:09:02 +0200

    Add navbar, multiple changes

--- src/pages/add-user.ts
+++ src/pages/add-user.ts
@@ -1,10 +1,13 @@
-import type { APIRoute } from 'astro';
+import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async ({ params, request }) =>  {
+export const POST: APIRoute = async ({ params, request }) => {
+  const body = await request.formData();
+  console.log(body);
+
   return new Response(
     JSON.stringify({
-      name: 'Astro',
-      url: 'https://astro.build/'
-    })
-  )
-}
+      name: "Astro",
+      url: "https://astro.build/",
+    }),
+  );
+};
