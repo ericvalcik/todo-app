5cbb6c54ecee2405b4a6c3ac9c94ba5271020e18
eric valcik <eric.valcik@gmail.com>
2023-09-16 14:54:37 +0200

    autoincrement ids schema change

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
-  id: integer("id").primaryKey(),
+  id: integer("id").primaryKey({ autoIncrement: true }),
   fullName: text("full_name"),
   phone: text("phone"),
 });
@@ -10,7 +10,7 @@ export type User = typeof users.$inferSelect; // return type when queried
 export type InsertUser = typeof users.$inferInsert; // insert type
 
 export const projects = sqliteTable("projects", {
-  id: integer("id").primaryKey(),
+  id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
   color: text("color").notNull().default("#000000"),
   archived: integer("archived", { mode: "boolean" }).notNull().default(false),
@@ -20,7 +20,7 @@ export type Project = typeof projects.$inferSelect; // return type when queried
 export type InsertProject = typeof projects.$inferInsert;
 
 export const todos = sqliteTable("todos", {
-  id: integer("id").primaryKey(),
+  id: integer("id").primaryKey({ autoIncrement: true }),
   description: text("description").notNull(),
   completed: integer("completed", { mode: "boolean" }).notNull().default(false),
   projectId: integer("project_id").references(() => projects.id),
